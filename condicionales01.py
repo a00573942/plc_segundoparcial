eb64fdad97076c729d8c4c4c5ecbbf40f6ea9854
a00573942 <145026211+a00573942@users.noreply.github.com>
2023-09-27 15:48:06 -0600

    Create condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,10 @@
+numero = int(input("ingresa el número: "))
+
+if (numero == 0):
+  print("tu número es igual a 0")
+
+elif (numero > 0):
+  print("tu número es positivo")
+else:
+  print("tu número es negativo")
+                   
